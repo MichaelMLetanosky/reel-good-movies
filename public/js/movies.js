7c595a7f4c18a1cb7b9099ae79b54f5607322fa8
GigaScott <scott.gamit.09@outlook.com>
2022-05-09 13:56:41 -0400

    updated TMDB API javascript

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,11 +1,12 @@
 
 const movieSearchHandler = function (event) {
     event.preventDefault();
-    
-    var movieTitle = document.getElementById("movie-search").value.trim();
+
+    var movieTitle = movieInputEl.value.trim();
+    console.log(movieTitle);
 
     if (movieTitle) {
-        getMovieRepos(movieTitle);
+        createMovieList(movieTitle);
         plotContainerEl.textContent = '';
     } else {
         alert('Please enter a movie title.');
@@ -15,12 +16,13 @@ const movieSearchHandler = function (event) {
 //Event Listener for when the search button is pressed
 movieFormEl.addEventListener('submit', movieSearchHandler);
 
-var getMovieRepos = function (movieTitle) {
+const getMovieRepos = function (movieTitle) {
 
-    var apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=a52d3acc220c86bfeeb8e86d712b26aa&query=${movieTitle}`
+    let apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=a52d3acc220c86bfeeb8e86d712b26aa&query=${movieTitle}`
 
-    // console.log(apiUrl);
+    console.log(apiUrl);
 
+    //API route tesing!
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
@@ -29,19 +31,17 @@ var getMovieRepos = function (movieTitle) {
                     // var movieTitle = document.getElementById("titleMe");
                     // var plot = document.getElementById("movie-plot");
                     //Notice the results[0] part! which means we need to pull a certain i of results for our app.
-                    const movieSearchdata = []
-                    for (let i = 0; i < 5; i++){
-                    const movieInfo = {
-                    movieTitle: data.results[i].original_title,
-                    plot: data.results[i].overview,
-                    moviePoster: `https://image.tmdb.org/t/p/w200/${data.results[i].poster_path}`,
+                    // const moviedata = [];
+                    const movieTitles = [];
+                    for (let i = 0; i < 6; i++) {
+                        const movieTitle = data.results[i].original_title;
+                        movieTitles.push(movieTitle);
+                        // plot: data.results[i].overview,
+                        // moviePoster: `https://image.tmdb.org/t/p/w200/${data.results[i].poster_path}`,
                     }
-                    movieSearchdata.push(movieInfo);
-                }
-                    console.log(moviedata);
+                    console.log(movieTitles);
+                    return movieTitles;
                 });
-
-            };
-
-        });
+        };
+});
 }
