9d11c7f6d9ac7ba8ecdf7b54fa632570063c8743
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-09 12:38:30 -0400

    Initial Set up of seeds index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,24 @@
 const sequelize = require('../config/connection');
+const { User, Movie, Review, UserMovie, FollowedUser } = require('../models');
+// TODO Seed files for 
+
+const userData = require('./userData.json');
+const movieData = require('./movieData.json');
+const reviewData = require('./reviewData.json');
+const userMovieData = require('./userMovieData.json');
+const followedUserData = require('./userData.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  //await INSERT SEED FUNCTION HERE
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // await Post.bulkCreate(postData)
+
+  // await Comment.bulkCreate(commentData)
  
   process.exit(0);
 };
