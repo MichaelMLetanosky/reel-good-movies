cfb389bed373e6e5fa12b80ee0ab0ab6d4ecaa14
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-09 10:44:48 -0400

    Added initial review model

---
+++ models/Review.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    movie_title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    tmdb_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
