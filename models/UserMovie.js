126daec333bf0e6213a9b8b91436fb5918d3b03e
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-09 10:57:37 -0400

    Added initial UserMovie model

---
+++ models/UserMovie.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class UserMovie extends Model {}
+
+UserMovie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user_movie',
+  }
+);
+
+module.exports = UserMovie;
