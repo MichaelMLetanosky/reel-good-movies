35efb42c18d342a7bd5682e55bace6658894e287
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-09 14:34:10 -0400

    Created Functionality to get, get by ID, put, and delete movies

--- controllers/api/movie-routes.js
+++ controllers/api/movie-routes.js
@@ -5,15 +5,99 @@ const { Movie, Review } = require('../../models');
 
 // get all movies
 router.get('/', async (req, res) => {
-  try {
-    const movieData = await Movie.findAll({
-      // be sure to include its associated Category and Tag data
-      include: [{ model: Review}]
-    });
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const movieData = await Movie.findAll({
+            include: [{ model: Review }]
+        });
+        res.status(200).json(movieData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get one movie
+router.get('/:id', async (req, res) => {
+    try {
+        const movieData = await Movie.findByPk(req.params.id, {
+            include: [{ model: Review }]
+        });
+
+        if (!movieData) {
+            res.status(404).json({ message: 'No movie found with this id!' });
+            return;
+        }
+
+        res.status(200).json(movieData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Post movie to db
+router.put('/:id', async (req, res) => {
+    try {
+        const movieData = await Movie.create(
+            {
+                movie_title: req.body.movie_title,
+                tmdb_id: req.body.tmdb_id,
+            }
+        );
+
+        if (!movieData) {
+            res.status(404).json({ message: 'No movie found with this id!' });
+            return;
+        }
+
+        res.status(200).json(movieData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Updates movie based on its id
+router.put('/:id', async (req, res) => {
+    try {
+        const movieData = await Movie.update(
+            {
+                movie_title: req.body.movie_title,
+                tmdb_id: req.body.tmdb_id,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+
+        if (!movieData) {
+            res.status(404).json({ message: 'No movie found with this id!' });
+            return;
+        }
+
+        res.status(200).json(movieData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const movieData = await Movie.destroy({
+            where: {
+                id: req.params.book_id,
+            },
+        });
+
+        if (!movieData) {
+            res.status(404).json({ message: 'No movie found with this id!' });
+            return;
+        }
+
+        res.status(200).json(movieData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
