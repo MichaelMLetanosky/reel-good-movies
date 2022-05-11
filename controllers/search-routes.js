ebafb9f5717320090e384c94635be49bb5e1f10e
GigaScott <scott.gamit.09@outlook.com>
2022-05-11 15:19:58 -0400

    search route is pulling information from API

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -1,72 +1,50 @@
 const router = require('express').Router();
-const axios = require('axios').default;
+const axios = require('axios');
+const { response } = require('express');
 
 router.get('/:movieTitle', (req, res) => {
- 
-    const getMovieRepos = function (movieTitle) {
 
-        let apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=a52d3acc220c86bfeeb8e86d712b26aa&query=Batman`
+    let apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=a52d3acc220c86bfeeb8e86d712b26aa&query=${req.params.movieTitle}`
+    axios.get(apiUrl)
+        .then( function (response) {
+            console.log("the api is working");
+            const movieTitles = [];
+            for (let i = 0; i < 6; i++) {
+                const movieTitle = response.data.results[i].original_title;
+                movieTitles.push(movieTitle);
+            }
+            // console.log(movieTitles);
+            // res.status(200).json(data);
+            res.render('moviecard',response)
+            let movieResults = getMovieDetails(movieTitles);
+            return movieResults;
+        });
 
-         axios.get(apiUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    return response.data;
-                    // (function (data) {
-                    //     console.log(data);
-                    // const movieTitles = [];
-                    // for (let i = 0; i < 6; i++) {
-                    //     const movieTitle = data.results[i].original_title;
-                    //     movieTitles.push(movieTitle);
-                    // }
-                    let movieResults = getMovieDetails(movieTitles);
-                    return movieResults;
-                    //     });
-                    // };
-                    // });
-                }
-            });
-        }
-        const moviesPlease = getMovieRepos(req.body.movieTitle)
-        res.status(200).json(moviesPlease);  
-    });
+    function getMovieDetails(data) {
+        const movieDetails = [];
+        movieData = data.forEach(function (movieTitle) {
+            let apiUrl = `https://www.omdbapi.com/?apikey=1d2b52b9&t=${movieTitle}`
+            axios.get(apiUrl)
+                .then( function (response) {
+                    let singleMovie = {
+                        title: response.data.Title,
+                        imdbId: response.data.imdbID,
+                        movie_poster: response.data.Poster,
+                        director: response.data.Director,
+                        runtime: response.data.Runtime,
+                        movie_rating: response.data.Rated,
+                        releaseDate: response.data.Released,
+                        actors: response.data.Actors,
+                        plot: response.data.Plot,
+                        review_rating: response.data.imdbRating,
+                    };
+                    movieDetails.push(singleMovie);
+                    console.log(movieDetails);
+                })
+        });
+    }
+});
 
-
-    //     function getMovieDetails(data) {
-    //         const movieDetails = [];
-    //         movieData = data.forEach(function (movieTitle) {
-
-    //             let apiUrl = `https://www.omdbapi.com/?apikey=1d2b52b9&t=${movieTitle}`
-
-    //             axios.get(apiUrl)
-    //                 .then(function (response) {
-    //                     if (response.ok) {
-    //                         response.json()
-    //                         .then(function (movie) {
-    //                             let singleMovie = {
-    //                                 title: movie.Title,
-    //                                 imdbID: movie.imdbID,
-    //                                 movie_poster: movie.Poster,
-    //                                 director: movie.Director,
-    //                                 runtime: movie.Runtime,
-    //                                 movie_rating: movie.Rated,
-    //                                 releaseDate: movie.Released,
-    //                                 actors: movie.Actors,
-    //                                 plot: movie.Plot,
-    //                                 review_rating: movie.imdbRating,
-    //                             };
-    //                             movieDetails.push(singleMovie);
-    //                         });
-    //                     };
-
-    //                 });
-    //         });
-    //         return movieDetails;
-    //     }
-        // const moviesPlease = getMovieRepos(req.params.movieTitle)
-        // res.status(200).json(moviesPlease);
-        // res.render('movieCard', movieDetails);
-
-
-    module.exports = router;
+module.exports = router;
 
 
