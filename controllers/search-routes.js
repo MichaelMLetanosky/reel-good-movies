707292c5851fbac8df1ad1595602efa660d66941
GigaScott <scott.gamit.09@outlook.com>
2022-05-11 17:07:21 -0400

    I'm sad

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -6,26 +6,29 @@ router.get('/:movieTitle', (req, res) => {
 
     let apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=a52d3acc220c86bfeeb8e86d712b26aa&query=${req.params.movieTitle}`
     axios.get(apiUrl)
-        .then( function (response) {
+        .then(function (response) {
             console.log("the api is working");
-            const movieTitles = [];
+            const movieTitlesArray = [];
             for (let i = 0; i < 6; i++) {
                 const movieTitle = response.data.results[i].original_title;
-                movieTitles.push(movieTitle);
+                movieTitlesArray.push(movieTitle);
             }
             // console.log(movieTitles);
             // res.status(200).json(data);
-            res.render('moviecard',response)
-            let movieResults = getMovieDetails(movieTitles);
+            // console.log(movieTitlesArray);
+            let movieResults = getMovieDetails(movieTitlesArray);
+            // console.log(movieResults);
+            // res.render('moviecard', singleMovie);
             return movieResults;
         });
 
-    function getMovieDetails(data) {
-        const movieDetails = [];
-        movieData = data.forEach(function (movieTitle) {
+    async function getMovieDetails(data) {
+
+        console.log(data);
+        movieData = await data.map((movieTitle) => {
             let apiUrl = `https://www.omdbapi.com/?apikey=1d2b52b9&t=${movieTitle}`
             axios.get(apiUrl)
-                .then( function (response) {
+                .then(function (response) {
                     let singleMovie = {
                         title: response.data.Title,
                         imdbId: response.data.imdbID,
@@ -37,11 +40,14 @@ router.get('/:movieTitle', (req, res) => {
                         actors: response.data.Actors,
                         plot: response.data.Plot,
                         review_rating: response.data.imdbRating,
-                    };
-                    movieDetails.push(singleMovie);
-                    console.log(movieDetails);
-                })
+                    }
+                    // console.log(singleMovie);
+                    return singleMovie;
+                })                     
+                .then()
         });
+        console.log(movieData);
+        return movieData;
     }
 });
 
