b0bb0546b136cbdde93969bfad2993dd53aa622c
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-11 23:14:18 -0400

    Checks if movie is in database, if it isn't adds to database

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const axios = require('axios');
+const { Movie } = require('../models');
 
 
 router.get('/:movieTitle', (req, res) => {
@@ -7,7 +8,7 @@ router.get('/:movieTitle', (req, res) => {
     axios.get(apiUrl)
         .then(function (response) {
             let singleMovie = {
-                title: response.data.Title,
+                movie_title: response.data.Title,
                 imdbId: response.data.imdbID,
                 movie_poster: response.data.Poster,
                 director: response.data.Director,
@@ -16,77 +17,35 @@ router.get('/:movieTitle', (req, res) => {
                 releaseDate: response.data.Released,
                 actors: response.data.Actors,
                 plot: response.data.Plot,
-                review_rating: response.data.imdbRating,
+                review_ratings: response.data.imdbRating,
             }
-            // console.log(movieTitles);
-            // res.status(200).json(data);
-            // const sadMovies = getMovieDetails(movieTitlesArray);
-            // let movieResults = movieTitlesArray.map(getMovieDetails())
-            // console.log(movieResults);
-            // console.log(movieResults);
-            res.render('singleMovie', singleMovie);
-            // return movieResults;
+
+            movieCheck(singleMovie);
         });
 
+    async function movieCheck(singleMovie) {
+        console.log('looking for db entry')
+        try {
+            const movieData = await Movie.findOne({
+                where: { movie_title: singleMovie.movie_title}
+            })
 
-//     function getMovieDetails(data) {
-//         // console.log(data);
-
-//         const movieData = Promise.all(data.map((movieTitle) => {
-//             let apiUrl = `https://www.omdbapi.com/?apikey=1d2b52b9&t=${movieTitle}`
-//             axios.get(apiUrl)
-//                 .then(function (response) {
-//                     // return response
-//                     let singleMovie = {
-//                         title: response.data.Title,
-//                         imdbId: response.data.imdbID,
-//                         movie_poster: response.data.Poster,
-//                         director: response.data.Director,
-//                         runtime: response.data.Runtime,
-//                         movie_rating: response.data.Rated,
-//                         releaseDate: response.data.Released,
-//                         actors: response.data.Actors,
-//                         plot: response.data.Plot,
-//                         review_rating: response.data.imdbRating,
-//                     }
-//                     // movieData.push(singleMovie);
-//                     // console.log(singleMovie);
-//                     // return singleMovie;
-//                 })
-//         })).then(
-//             console.log(movieData);
-//         return movieData;
-//         )
-//     }
-});
+            if (movieData) {
+                console.log('found entry')
+                res.render('singleMovie', singleMovie);
+            } else {
+                console.log('no entry found')
+                Movie.create(singleMovie)
+                res.render('singleMovie', singleMovie)
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    };
 
 
-// async function getMovieDetails(data) {
+        
+    });
 
-//         let apiUrl = `https://www.omdbapi.com/?apikey=1d2b52b9&t=${data}`
-//         axios.get(apiUrl)
-//             .then(function (response) {
-//                 // return response
-//                 let singleMovie = {
-//                     title: response.data.Title,
-//                     imdbId: response.data.imdbID,
-//                     movie_poster: response.data.Poster,
-//                     director: response.data.Director,
-//                     runtime: response.data.Runtime,
-//                     movie_rating: response.data.Rated,
-//                     releaseDate: response.data.Released,
-//                     actors: response.data.Actors,
-//                     plot: response.data.Plot,
-//                     review_rating: response.data.imdbRating,
-//                 }
-//                 // console.log(singleMovie);
-//                 return singleMovie;
-//             })   
-//         }                  
-
-
-
-
-
-
+        
 module.exports = router;
