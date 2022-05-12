4d058fd4e5247f4ac609f1e48e9f2c1e7b60ed41
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-05-12 11:29:24 -0400

    Fixed localhost bug

--- config/connection.js
+++ config/connection.js
@@ -8,13 +8,15 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-    },
+    }
   );
 }
+
+
 
 module.exports = sequelize;
